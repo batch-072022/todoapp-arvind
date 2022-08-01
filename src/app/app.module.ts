64e1f90d841f4adaf0bfc6977949ec27e1d16bd8
Arvind Karthik <arvind.karthik@valuemomentum.com>
2022-08-01 18:22:25 +0530

    commiting workspace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AddtaskComponent } from './addtask/addtask.component';
+import { ListtaskComponent } from './listtask/listtask.component';
+import { EdittaskComponent } from './edittask/edittask.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddtaskComponent,
+    ListtaskComponent,
+    EdittaskComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
